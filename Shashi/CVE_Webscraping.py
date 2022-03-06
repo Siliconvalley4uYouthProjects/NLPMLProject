f5457438c8c2d755f0d6d8ab4733e4774d572ce1
Suram Anugna <anugnasuram1994@gmail.com>
2022-03-06 18:41:29 +0530

    Add files via upload

---
+++ Shashi/CVE_Webscraping.py
@@ -0,0 +1,60 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import csv
+import numpy as np
+
+##################################################
+headers = dict()
+headers[
+    "User-Agent"
+] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+###################################################
+# Defining of the dataframe
+#, 'CWE_ID','No_of_Exploits','Vulnerability_Type(s)','Publish_Date','Update_Date','Score','Gained_Access_Level','Access','Complexity','Authentication','Conf','Integ','Avail'])
+df = pd.DataFrame(columns=['S_no', 'CVE_ID'])
+pages = np.arange(1, 10, 1)
+for page in pages:
+    url = "https://www.cvedetails.com/vulnerability-list.php?vendor_id=16&product_id=&version_id=&page=1&hasexp=0&opdos=0&opec=0&opov=0&opcsrf=0&opgpriv=0&opsqli=0&opxss=0&opdirt=0&opmemc=0&ophttprs=0&opbyp=0&opfileinc=0&opginf=0&cvssscoremin=0&cvssscoremax=0&year=0&month=0&cweid=0&order=1&trc=4159&sha=b6b9f0966b7dbca88b729e5b85a1f8fffc37d986"
+    results = requests.get(url, headers=headers)
+    soup = BeautifulSoup(results.text, "html.parser")
+    table = soup.find('table', class_="searchresults sortable", id="vulnslisttable")
+    print(page)
+###################################################
+table1 = soup.find('table', class_="searchresults sortable", id="vulnslisttable")
+# print("hi.........")
+# print(table1)
+
+# Obtain every title of columns with tag <th>
+headers = []
+for i in table1.find_all('th'):
+    title = i.text
+    headers.append(title)
+    # print(headers)
+
+headers.append('\n\t\t\t\tDesc.\n\t\t\t')
+mydata = pd.DataFrame(columns = headers)
+print(mydata)
+
+
+# Create a for loop to fill mydata
+temp_row = []
+count = 0
+for j in table1.find_all('tr')[1:]: # need the data in this tag
+    count += 1
+    row_data = j.find_all('td')
+    row = [i.text for i in row_data]
+    if count % 2 != 0:
+        temp_row = []
+    temp_row += row
+
+    if count % 2 == 0:
+        print(temp_row)
+
+    #length = len(mydata)
+    #mydata.loc[length] = row
+
+
+
+
+
